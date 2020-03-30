097b703d7145aa89137d4b76c58b9f3c5d0ec413
TAYLOR GEORGE <ATGEORGE@UTEXAS.EDU>
2020-03-30 18:40:53 -0500

    finish auth

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     """Register a new user"""
+    print("Made it here in the register pathway")
     if request.method != 'POST':
         #Display blank registration form
         form = UserCreationForm()
@@ -17,8 +18,17 @@ def register(request):
             login(request, new_user)
             return redirect('learning_logs:index')
 
-        #Display a blank or invalid form
-        context = {'form': form}
-        return render(request, 'registration/register.html', context)
+    #Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
 
+def login(request):
+    """Login page"""
+    print("Made it here in login")
+    return render(request, 'registration/login.html')
+
+def logout(request):
+    """Logout page"""
+    print("Made it here")
+    return render(request, 'registration/logout.html')
 
